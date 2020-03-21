ff7de61e39cb1b6724c6de8c47c0ed5270c8bce2
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2020-03-21 16:47:38 +0530

    Feature: Elevate a country on hover & get info

--- src/components/Globe.js
+++ src/components/Globe.js
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Globe from 'react-globe.gl';
 
 import earthNight from '../assets/earth-night.jpg';
 import nightSky from '../assets/night-sky.png';
+import worldMap from '../datasets/world.geojson';
 
 export default ({
 }) => {
   const globeEl = React.useRef();
 
-  React.useEffect(() => {
+  const [countries, setCountries] = useState({ features: [] });
+  const [hoverD, setHoverD] = useState();
+
+  useEffect(() => {
     globeEl.current.controls().autoRotate = true;
     globeEl.current.controls().autoRotateSpeed = 0.1;
+
+    fetch(worldMap).then(res => res.json()).then(setCountries);
   }, [true]);
 
   return (
     <Globe
       ref={globeEl}
+      animateIn
+      waitForGlobeReady
       globeImageUrl={earthNight}
       backgroundImageUrl={nightSky}
+
+      polygonsData={countries.features}
+      polygonAltitude={d => d === hoverD ? 0.12 : 0.06}
+      polygonCapColor={d => d === 'rgb(255,255,255)'}
+      polygonSideColor={() => 'rgba(0, 100, 0, 0.15)'}
+      polygonStrokeColor={() => '#111'}
+      polygonLabel={({ properties: d }) => `
+        <b>${d.ADMIN} (${d.ISO_A2}):</b> <br />
+        GDP: <i>${d.GDP_MD_EST}</i> M$<br/>
+        Population: <i>${d.POP_EST}</i>
+      `}
+      onPolygonHover={setHoverD}
+      polygonsTransitionDuration={300}
     />
   );
 };
