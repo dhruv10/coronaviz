d0f04c0e4098eb7bc724d157eccf287796366c38
Dhruv Bhatnagar <dhruvbhatnagar10@gmail.com>
2020-03-22 20:38:06 +0530

    Fixed: Removed unneccessary navbar items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,8 @@ const Navbar = () => {
     <React.Fragment>
       <nav className='navbar' role='navigation' aria-label='main navigation'>
         <div className='navbar-brand'>
-          <a className='navbar-item' href='https://bulma.io'>
-            <img
-              src='https://bulma.io/images/bulma-logo.png'
-              width='112'
-              height='28'
-            />
+          <a className='navbar-item title' href='#'>
+            CORONAViz
           </a>
 
           <a
@@ -28,11 +24,11 @@ const Navbar = () => {
 
         <div id='navbarBasicExample' className='navbar-menu'>
           <div className='navbar-start'>
-            <a className='navbar-item'>Home</a>
+            <a className='navbar-item active'>Globe</a>
 
-            <a className='navbar-item'>Documentation</a>
+            {/* <a className='navbar-item'>Documentation</a> */}
 
-            <div className='navbar-item has-dropdown is-hoverable'>
+            {/* <div className='navbar-item has-dropdown is-hoverable'>
               <a className='navbar-link'>More</a>
 
               <div className='navbar-dropdown'>
@@ -42,16 +38,16 @@ const Navbar = () => {
                 <hr className='navbar-divider' />
                 <a className='navbar-item'>Report an issue</a>
               </div>
-            </div>
+            </div> */}
           </div>
 
           <div className='navbar-end'>
             <div className='navbar-item'>
               <div className='buttons'>
-                <a className='button is-primary'>
+                {/* <a className='button is-primary'>
                   <strong>Sign up</strong>
                 </a>
-                <a className='button is-light'>Log in</a>
+                <a className='button is-light'>Log in</a> */}
               </div>
             </div>
           </div>
